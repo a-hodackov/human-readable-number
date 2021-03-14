eeeb9f0257a5a3c62f9a0fa9c0c57396b02f8500
a-hodackov <hodackoff1995@gmail.com>
2021-03-15 00:09:22 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const num = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "",
+        "twenty ",
+        "thirty ",
+        "forty ",
+        "fifty ",
+        "sixty ",
+        "seventy ",
+        "eighty ",
+        "ninety ",
+    ];
+    const hundredsDigit = Math.floor(number / 100); //3
+    let tensDigit = Math.floor((number - hundredsDigit * 100) / 10); //5
+    let onesDigit = number - hundredsDigit * 100 - tensDigit * 10; //1
+    if (number === 0) return "zero";
+    if (tensDigit === 1) {
+        tensDigit = 0;
+        onesDigit = number - hundredsDigit * 100;
+    }
+    const hundreds = hundredsDigit ? num[hundredsDigit] + " hundred " : "";
+    return (hundreds + tens[tensDigit] + num[onesDigit]).trim();
+};
